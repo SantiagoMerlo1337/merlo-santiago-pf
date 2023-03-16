2f63fd9bbf6d599e5e266f88ed3f3d10c7cb1c77
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2023-03-16 01:22:06 -0300

    html to php

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <!-- Meta -->
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Curriculum Vitae de: Merlo, Santiago.">
-    <meta name="author" content="Merlo, Santiago">
-    <meta name="keywords" content="cv, curriculumvitae, fullstack, frontend, webdeveloper">
-    <title>Merlo Santiago</title>
-
-    <!-- Links -->
-    <link rel="stylesheet" href="css/reducido.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <?php
+        require_once('resources/head.php');
+    ?>
 </head>
 
 <body class="bg-dark">
-
     <!-- Header -->
     <div class="container-fluid bg-dark shadow">
         <div class="container">
