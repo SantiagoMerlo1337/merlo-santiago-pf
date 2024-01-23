5a32e7f926dce42fca9dd5828747e82d5ff7fb7d
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-01-23 19:02:06 -0300

    fix contacto.tsx

--- components/contacto/contacto.tsx
+++ components/contacto/contacto.tsx
@@ -4,13 +4,13 @@ const Contacto = () => {
     return (
         <section className="text-center lg:text-start">
             <h3 className="h3 ">Contacto</h3>
-            <ul className="list-none">
+            <ul className="list-none pb-2">
                 <li className="mb-1">Rafaela, Santa Fe, Argentina</li>
                 <li className="mb-1">santiagomerlo1337@gmail.com</li>
                 <li className="mb-1">linkedin.com/in/santiagomerlo1337</li>
                 <li className="mb-1">+5403492691132</li>
             </ul>
-            <ul className="flex gap-2">
+            <ul className="flex gap-2 items-center justify-center lg:items-start lg:justify-start">
                 <RedSocial
                     href="https://github.com/SantiagoMerlo1337"
                     color="purple"
