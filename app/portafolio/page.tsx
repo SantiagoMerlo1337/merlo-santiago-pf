1b8d95fc92379e3c4465f44921396ac30f5d7d2e
Santiago Merlo <66649631+SantiagoMerlo1337@users.noreply.github.com>
2024-01-29 17:53:52 -0300

    Improve trabajo.tsx and "/portafolio" page

--- app/portafolio/page.tsx
+++ app/portafolio/page.tsx
@@ -1,16 +1,31 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Fragment } from "react";
 import Trabajo from "@/components/trabajo/trabajo";
 import dataPortafolio from "../../assets/data/portafolio.json";
+import imgFlechaVolver from "../../public/img/flecha-izquierda.png"
 
 const Portafolio = () => {
     return (
-        <div className="bg-blue-400">
-            Portafolio
-            {/* <div className="fs-1">Portafolio</div>
-            {dataPortafolio.map((item, index) => (
-                <Trabajo key={index} items={item} />
-            ))} */}
-        </div>
+        <Fragment>
+            <Link href="/" className="float-left rounded-full bg-gray-600 hover:bg-gray-700 p-2 mt-9">
+                <Image src={imgFlechaVolver} alt="Icono flecha volver" width={30} className="invert"></Image>
+            </Link>    
+            <div className="container-portafolio">
+                {dataPortafolio.map((props, index) => (
+                    <Trabajo
+                    key={index}
+                    title={props.title}
+                    desc={props.desc}
+                    tech={props.tech}
+                    site={props.site}
+                    github={props.github}
+                    />
+                ))}
+            </div>
+        </Fragment>
+        
     );
 };
 
