b3eb185e479c63ee38adfe21200ec1a080ab4805
MNAF1337 <66649631+MNAF1337@users.noreply.github.com>
2024-01-30 16:18:59 -0300

    some changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+        // Agregar configuración para manejar archivos PDF
+        config.module.rules.push({
+            test: /\.(pdf)$/,
+            use: [
+                {
+                    loader: "file-loader",
+                    options: {
+                        // Nombre de salida para los archivos PDF
+                        name: "[name].[ext]",
+                        // Directorio de salida para los archivos PDF en la carpeta pública
+                        outputPath: "files",
+                        // Habilitar la emisión de URL relativas
+                        publicPath: "_next/static/files/",
+                    },
+                },
+            ],
+        });
+
+        // Necesario para pasar el isServer flag a otras configuraciones de Webpack
+        if (!isServer) {
+            config.node = {
+                fs: "empty",
+            };
+        }
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
