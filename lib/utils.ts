fdc7644954d7e6c0b8078d2333d6c270fb00d41f
SantiagoMerlo1337 <santiagomerlo1337@gmail.com>
2024-08-18 00:54:16 -0300

    update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function textTypingEffect(element:any, text:string, i = 0) {
+  if( i === 0){
+    element.textContext = "";
+  }
+  
+  element.textContext += text[i];
+
+  if (i === text.length - 1) {
+    return
+  }
+
+  setTimeout(()=> textTypingEffect(element, text, i + 1), 550)
+}
