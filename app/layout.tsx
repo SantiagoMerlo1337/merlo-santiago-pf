2849d7e20e0d5e539f4582e4d52b1a76adf56afd
SantiagoMerlo1337 <santiagomerlo1337@gmail.com>
2024-08-19 17:38:17 -0300

    improve metadata, add preview image and remove old background image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Roboto_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
-import Image from "next/image";
 
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
@@ -22,7 +21,39 @@ export const metadata: Metadata = {
         "Portafolio",
         "Santiago Merlo",
         "Curriculum Vitae",
-    ]
+        "Desarrollador Fullstack",
+        "React",
+        "Next.js",
+        "Node.js",
+        "JavaScript",
+        "MongoDB",
+        "PostgreSQL",
+        "CSS",
+        "HTML",
+        "Frontend Developer",
+        "Backend Developer",
+        "Desarrollo Web",
+        "Aplicaciones Web",
+        "Programación",
+    ],
+    applicationName: "Santiago Merlo Portfolio",
+    robots: "index, follow",
+    themeColor: "#000000",
+    viewport: "width=device-width, initial-scale=1.0",
+    openGraph: {
+        title: "santiagoMerlo dev",
+        description: "Soy un desarrollador web fullstack con experiencia en diversas tecnologías y habilidades relacionadas con el desarrollo de aplicaciones web y la programación.",
+        url: "https://merlosantiago-pf.vercel.app/",
+        siteName: "santiagoMerlo dev",
+        images: [
+            {
+                url: "https://github.com/SantiagoMerlo1337/merlo-santiago-pf/blob/main/public/assets/images/preview.png",
+                width: 1200,
+                height: 630,
+                alt: "Santiago Merlo Portafolio",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
